6696c0b158dd48ae4694ee659a2e6157dddd0f11
Jim Leaf <jimleaf@doesnotexistanywhere123.com>
2025-12-01 18:19:26 -0800

    using steamlit

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+import streamlit as st
+import streamlit.components.v1 as components
+import os
 
-app = Flask(__name__, static_folder='static', template_folder='.')
+# Set page config
+st.set_page_config(page_title="Horse Details", layout="wide")
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+# Load the HTML file
+html_path = os.path.join(os.path.dirname(__file__), "index.html")
+with open(html_path, "r", encoding="utf-8") as f:
+    html_content = f.read()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Render the HTML
+components.html(html_content, height=800, scrolling=True)
